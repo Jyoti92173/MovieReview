659f8064f8e058270045f3269772615faeb7c7fa
jyoti92173 <jyoti53913@gmail.com>
2024-12-28 14:50:22 +0530

    Implement MovieResponse DTO to represent movie details in response

---
+++ src/main/java/com/app/MovieReview/service/response/MovieResponse.java
@@ -0,0 +1,23 @@
+package com.app.MovieReview.service.response;
+
+import com.app.MovieReview.domain.Genre;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieResponse {
+    private String title;
+    private Genre genre;
+    private Double rating;
+    private List<ReviewResponse> reviews;
+
+    public MovieResponse(String title, Genre genre, Double rating, List<ReviewResponse> reviewResponse) {
+        
+    }
+}
